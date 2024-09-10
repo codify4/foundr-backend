3de824d6209af7fb49f37d6f4828caf386029d3e
IjonK007 <kushta.joni@gmail.com>
2024-09-10 17:48:17 +0200

    added dashboard and page module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { DashboardModule } from './dashboard/dashboard.module';
+import { PageModule } from './page/page.module';
 
 @Module({
-  imports: [AuthModule, PrismaModule],
+  imports: [AuthModule, PrismaModule, DashboardModule, PageModule],
 })
 export class AppModule {}
