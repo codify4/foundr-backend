f382328504d29272d532c17ffef49477ee3d6738
codify4 <kushta.joni@gmail.com>
2024-09-17 20:06:31 +0200

    small changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
-import { DashboardModule } from './dashboard/dashboard.module';
 import { PageModule } from './page/page.module';
 import { SocialsModule } from './socials/socials.module';
 import { ProjectsModule } from './projects/projects.module';
@@ -10,6 +9,6 @@ import { GithubCommitsModule } from './github-commits/github-commits.module';
 import { XPostsModule } from './x-posts/x-posts.module';
 
 @Module({
-  imports: [AuthModule, PrismaModule, DashboardModule, PageModule, SocialsModule, ProjectsModule, UserModule, GithubCommitsModule, XPostsModule],
+  imports: [AuthModule, PrismaModule, PageModule, SocialsModule, ProjectsModule, UserModule, GithubCommitsModule, XPostsModule],
 })
 export class AppModule {}
