9b3f1b483cf06bbfd2c6e1100d0d1c5d63b37766
IjonK007 <kushta.joni@gmail.com>
2024-09-18 16:40:41 +0200

    test endpoint added

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,18 @@
-import { Body, Controller, HttpCode, HttpStatus, Patch, UseGuards, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpStatus, Patch, UseGuards, ValidationPipe } from '@nestjs/common';
 import { AtGuard } from 'src/auth/guards/at.guard';
 import { GetCurrentUserId } from 'src/common/decorators/get-current-user-id.decorator';
 import { UserService } from './user.service';
 import { updateUserDto } from './dto/update-email.dto';
 
-@UseGuards(AtGuard)
+//@UseGuards(AtGuard)
 @Controller('user')
 export class UserController {
     constructor(private userService: UserService) {}
+
+    @Get('test')
+    test() {
+        return 'LFG';
+    }
 
     @HttpCode(HttpStatus.OK)
     @Patch('updateUser')
